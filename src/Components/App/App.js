7edceb199070fcfde87c318e235299510cefd83b
Bea <103050835+Beadko@users.noreply.github.com>
2022-08-25 18:59:45 +0700

    Added a componentDidMount method to get the access token as soon as the page loads. This have fixed the double loading issue

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -58,6 +58,10 @@ class App extends React.Component {
 
   }
 
+  componentDidMount() {
+   window.addEventListener('load', () => {Spotify.getAccessToken()});
+ }
+
   render() {
     return(
      <div>
